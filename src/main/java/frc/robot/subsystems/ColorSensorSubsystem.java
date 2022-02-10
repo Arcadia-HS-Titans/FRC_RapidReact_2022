f64603e01a2b6921e9cbd112842ba6372fbadb93
Athan <aparkis00@susdgapps.org>
2022-02-10 16:25:45 -0700

    Color sensor beginning

---
+++ src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ColorSensorSubsystem extends SubsystemBase {
+    private final ColorSensorV3 colorSensor;
+
+    public ColorSensorSubsystem(ColorSensorV3 colorSensor) {
+        // How to initialize for later: Use an IC2 port: I2cPort.kOnboard
+        // https://github.com/REVrobotics/Color-Sensor-v3-Examples/blob/master/Java/Read%20RGB%20Values/src/main/java/frc/robot/Robot.java
+        //https://youtu.be/dQw4w9WgXcQ
+        // https://www.revrobotics.com/rev-31-1557/ Color sensor page
+
+        this.colorSensor = colorSensor;
+    }
+
+    public Color getColor() {
+        return colorSensor.getColor();
+    }
+
+}
