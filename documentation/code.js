6c2e6f89eee078eaf80a42b42c25deee18f52823
Athan <aparkis00@susdgapps.org>
2022-02-26 22:58:55 -0700

    Start of actual docs website and how they will be structured

---
+++ documentation/code.js
@@ -0,0 +1,15 @@
+function sensor1() {
+    document.getElementById("info").innerHTML = "Sensor 1 explanation"
+}
+
+function sensor2() {
+    document.getElementById("info").innerHTML = "Sensor 2 explanation fwefefwf"
+}
+
+function sensor3() {
+    document.getElementById("info").innerHTML = "Sensor 3 explanation ff34"
+}
+
+function sensor4() {
+    document.getElementById("info").innerHTML = "Sensor 4 explanation 434g43g45g 54g 54ggh54h5"
+}
