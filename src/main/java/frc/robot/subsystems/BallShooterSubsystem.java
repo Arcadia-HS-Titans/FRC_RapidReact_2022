e7c3b8b8fbec63384b229c5a467e567f0a21d753
Athan <aparkis00@susdgapps.org>
2022-03-16 10:49:52 -0700

    Ball Shooter Encoders

--- src/main/java/frc/robot/subsystems/BallShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/BallShooterSubsystem.java
@@ -2,8 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -11,6 +10,7 @@ import frc.robot.Constants;
 public class BallShooterSubsystem extends SubsystemBase {
     private final CANSparkMax canBus;
     private final PWMVictorSPX susanMotor;
+    private final RelativeEncoder encoder;
     //private final DigitalInput magSwitch;
 
     private double rotationCounter = 0;
@@ -18,11 +18,20 @@ public class BallShooterSubsystem extends SubsystemBase {
     public BallShooterSubsystem() {
         this.canBus = new CANSparkMax(12, CANSparkMaxLowLevel.MotorType.kBrushless); // TODO: Change device ID to when it's set up
         this.susanMotor = new PWMVictorSPX(Constants.SUSAN_ROTATION_PORT);
+        this.encoder = canBus.getEncoder();
         //this.magSwitch = new DigitalInput(Constants.MAGNETIC_SWITCH_PORT);
     }
 
     public void fire(double powerPercent) {
         canBus.set(powerPercent);
+    }
+
+    public RelativeEncoder getEncoder() {
+        return encoder;
+    }
+
+    public double getVelocity() {
+        return encoder.getVelocity();
     }
 
     public void setInverted(boolean value) {
